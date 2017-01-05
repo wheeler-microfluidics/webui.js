b29d93da32bf343d9fc3863dae849eef1fb669b7
Christian Fobel <christian@fobel.net>
2017-01-05 10:26:17 -0500

    Add missing file

---
+++ test/libDeviceUIPlugin.js
@@ -0,0 +1,56 @@
+/*
+ * Currently, ``browserify-shim`` is used for ``two.js``, as described [here][1].
+ * Support for ``browserify`` [has been added on the ``dev`` branch][2].
+ *
+ * **TODO** Switch to using ``two.js`` node package directly once [this
+ * commit][3] on the ``dev`` branch of ``two.js`` is released in the mainline
+ * package.
+ *
+ * [1]: https://github.com/jonobr1/two.js/issues/43#issuecomment-250081235
+ * [2]: https://github.com/jonobr1/two.js/issues/43#issuecomment-250240820
+ * [3]: https://github.com/jonobr1/two.js/commit/bf60571398f84f71c40eaf438650fa5ec3275784
+ */
+global.Plotly = require('plotly.js');
+global.Ajv = require('ajv');
+global.Two = require('two');
+global.dat = require('dat.gui');
+global.io = require('socket.io-client');
+global._ = require('lodash');
+global._fp = require('lodash/fp');
+global.THREE = require('three');
+global.Key = require('keyboard-shortcut');
+global.PhosphorMenus = require('phosphor-menus');
+global.PhosphorWidget =require('phosphor-widget');
+
+global.THREELine2d = {}
+global.THREELine2d.Line = require('../')(THREE);
+global.THREELine2d.BasicShader = require('../shaders/basic')(THREE);
+global.THREELine2d.DashShader = require('./shader-dash')(THREE);
+global.THREELine2d.GradientShader = require('./shader-gradient')(THREE);
+
+global.THREELine2d.normalize = require('normalize-path-scale');
+global.THREELine2d.arc = require('arc-to');
+global.THREELine2d.curve = require('adaptive-bezier-curve');
+
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/css/css');
+require('codemirror/mode/htmlmixed/htmlmixed');
+
+global.CodeMirror = require('codemirror/lib/codemirror');
+global.$ = require("jquery-slim");
+global.d3 = require("d3");
+global.OrbitControls = require('three-orbit-controls')(THREE);
+global.Stats = require("stats-js");
+global.DockPanel = require('phosphor-dockpanel').DockPanel;
+
+global.MouseEventHandler =
+    require("../ThreeHelpers.MouseEventHandler.js")(THREE);
+global.ThreeHelpers =
+    require("../ThreeHelpers.SvgPathsGroup.js")(THREE);
+global.ZmqPlugin = require("../zmq-plugin.js");
+global.ControlPointsUI = require("../d3.controlPoints.js");
+global.PerspectiveTransform = require("../PerspectiveTransform.js");
+global.PlaneTransform = require("../planeTransform.js")(THREE);
+global.DataFrame = require("../pandas-helpers.js").DataFrame;
+global.Widgets = require("../widgets.js")(THREE);
